c2efb677479d0776e221143bac603eda36395831
Robert-GIT <tilahizafyrobertojudicael@gmail.com>
2024-11-10 21:41:06 +0300

    Troisième mis à jour

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Notre code JavaScript ici
+  console.log("here");
+
+  const showContactBtn = document.getElementById("showContact");
+  const closeContactBtn = document.getElementById("closeContactBtn");
+  const contactBlock = document.getElementById("contactBlock");
+
+  // Afficher le bloc contact
+  showContactBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    contactBlock.classList.add("show");
+    console.log("clicked");
+  });
+
+  // Cacher le bloc contact
+  closeContactBtn.addEventListener("click", () => {
+    contactBlock.classList.remove("show");
+    console.log("clicked");
+  });
+});
+// Sélectionner les éléments
